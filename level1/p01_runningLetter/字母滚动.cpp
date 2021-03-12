33aec6a117ec304c97f7019fc0acef1d267330ae
徐梓铭 <xzm2002@126.com>
2021-03-12 21:28:18 +0800

    Signed-off-by: 徐梓铭 <xzm2002@126.com>

---
+++ level1/p01_runningLetter/字母滚动.cpp
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<windows.h>
+#include<unistd.h>
+#define H 15
+#define W 20
+int c[H][W] = {0};
+int i,j,k;
+void ain()
+{
+	
+	for (i=0;i<H;i++)
+	{
+		c[i][0] = -1;
+		c[i][W-1] =- 1;
+	}
+	for (j=0;j<W;j++)
+	{
+		c[0][j] =- 1;
+		c[H-1][j] = -1;
+	}
+	for(int z=1;z<W-1;z++)
+	{
+		c[1][1]=1;
+	}
+}
+void m()
+{
+	
+	for (i=0;i<H;i++)
+	{
+		for (j=0;j<W;j++)
+		{
+			if (c[i][j]==0)
+				printf(" ");   
+			else if (c[i][j]==-1)
+				printf("#");
+			else printf("A");
+		}
+		printf("\n");
+	}
+ }
+
+int main()
+{
+	ain();
+	m();
+		for(int u=0;;)	
+			{
+				if(u==0)
+				{
+				for(int z=1;z<18;z++)
+				{
+				sleep(0.1);
+				system("cls");
+				c[1][z]=0;
+				c[1][z+1]=1;
+				m();
+				}
+			u=1;
+			}
+		if(u==1)
+		{
+		for(int h=18;h>1;h--)
+			{
+			sleep(0.2);
+			system("cls");
+			c[1][h]=0;
+			c[1][h-1]=1;
+				m();
+		}
+		u=0;
+	}		
+}
+return 0;	
+}
