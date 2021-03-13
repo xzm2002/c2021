320bcaad46579909ba0634954669889543e795f5
徐梓铭 <xzm2002@126.com>
2021-03-14 00:40:17 +0800

    Signed-off-by: 徐梓铭 <xzm2002@126.com>

--- level1/p02_isPrime/判断n是否为素数.c
+++ level1/p02_isPrime/判断n是否为素数.c
@@ -3,16 +3,16 @@
 int main()
 {
 	int n,i,m;
-	int a[1000];
+
 	scanf("%d",&n);
 	if(n==2)  printf("yes");
 	else if(n>2)
 	{
 		for(i=2;i<n;i++)
 		{
-			if(n%i==0) {a[i]=1; break;}
+			if(n%i==0) {m=1; break;}
 		}
-		if(a[i]==1) printf("no");
+		if(m==1) printf("no");
 		else printf("yes");
 	}
 	else printf("error");
